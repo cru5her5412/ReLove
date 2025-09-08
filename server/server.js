1ee31cb6f156f78d566c08a6e296e905c644e083
Callum James Lodge <callumjlodge@gmail.com>
2025-09-08 14:39:49 +0100

    added setup code to server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
-import dotenv from "dotenv";
 import cors from "cors";
-import express from "express";
+import express, { text } from "express";
+import { db } from "./dbConnections.js";
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.listen(8080, function () {});
