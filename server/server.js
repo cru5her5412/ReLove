70d60064abaa5a40acbd651a2b42f19fea2b4a27
Callum James Lodge <callumjlodge@gmail.com>
2025-09-08 15:29:52 +0100

    added get functions for 3 sites(homePage, submitItem and viewItems)

--- server/server.js
+++ server/server.js
@@ -4,4 +4,19 @@ import { db } from "./dbConnections.js";
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.listen(8080, function () {});
+const port = 8080;
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
+app.get("/", function (request, response) {
+  homePage(request, response);
+});
+app.get("/submit-item", function (request, response) {
+  submitItem(request, response);
+});
+app.get("/view-items", async function (request, response) {
+  viewItems(request, response);
+});
+async function homePage() {}
+async function submitItem() {}
+async function viewItems() {}
